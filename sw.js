9a7ee3c3057b7e35c8a2a6fab1b999c05b8ceab2
Bernd Klos <bernd.klos@icloud.com>
2026-07-07 21:10:11 +0200

    Fix Fahrer Dropdown Ausrichtung v0.2.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "schlaglink-v3";
+const CACHE_VERSION = "farm-manager-v0-2-2";
 const APP_CACHE = `${CACHE_VERSION}-app`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const APP_SHELL = [
